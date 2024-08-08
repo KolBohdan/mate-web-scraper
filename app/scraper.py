707501eaf2d6bbd88d6bbf978375cc131b97864c
Bohdan Kolomiiets <kb907406@gmail.com>
2024-08-08 08:48:13 +0300

    Implement parse_course_with_types method with additional methods

--- app/scraper.py
+++ app/scraper.py
@@ -56,3 +56,67 @@ class MateScraper:
             num_of_topics=num_of_topics,
             duration=duration,
         )
+
+    @staticmethod
+    def get_button_of_course_type(
+        course: WebElement, course_type: CourseType
+    ) -> WebElement | None:
+        course_type_buttons = course.find_elements(By.TAG_NAME, "a")
+        for button in course_type_buttons:
+            if button.text == course_type.value:
+                return button
+
+    @staticmethod
+    def update_course(course_name: str) -> WebElement:
+        courses = driver.find_elements(By.CLASS_NAME, COURSES_CSS_SELECTOR)
+
+        for course in courses:
+            if course.find_element(By.TAG_NAME, "h3").text == course_name:
+                return course
+
+    def parse_course_with_types(self, course: WebElement) -> list[Course]:
+        courses = []
+
+        course_name = course.find_element(By.TAG_NAME, "h3").text
+        course_description = course.find_element(By.CLASS_NAME, "mb-32").text
+
+        for course_type in [CourseType.FLEX, CourseType.FULL_TIME]:
+            button = self.get_button_of_course_type(
+                course=course, course_type=course_type
+            )
+
+            if button is None:
+                continue
+
+            href = button.get_attribute("href")
+            driver.get(href)
+
+            num_of_modules = int(
+                driver.find_element(
+                    By.CLASS_NAME, MODULES_NUMBER_CSS_SELECTOR
+                ).text.split()[0]
+            )
+            num_of_topics = int(
+                driver.find_element(
+                    By.CLASS_NAME, TOPICS_NUMBER_CSS_SELECTOR
+                ).text.split()[0]
+            )
+            duration = driver.find_element(
+                By.CLASS_NAME, COURSE_DURATION_CSS_SELECTOR
+            ).text
+
+            courses.append(
+                self.create_course_model(
+                    course_name=course_name,
+                    course_description=course_description,
+                    course_type=course_type,
+                    num_of_modules=num_of_modules,
+                    num_of_topics=num_of_topics,
+                    duration=duration,
+                )
+            )
+
+            driver.back()
+            course = self.update_course(course_name=course_name)
+
+        return courses
